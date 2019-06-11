313cf53f1877f7abf1d9ca42404fbbfa43911ae2
Lorenzo Raimondi <lorenzo.raimondi@eni.com>
2019-06-11 12:37:13 +0200

    some fixing

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,7 +4,6 @@ from django.utils import timezone
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 
-
 from .models import Question
 
 class IndexView(generic.ListView):
@@ -32,6 +31,7 @@ class DetailView(generic.DetailView):
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
+
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
